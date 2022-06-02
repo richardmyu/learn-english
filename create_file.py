928dc45a4cd1c11813c075e485d7506f00316385
yum <richardminyu@foxmail.com>
2022-06-02 16:52:12 +0800

    update

--- create_file.py
+++ create_file.py
@@ -15,7 +15,10 @@ LINUX_WORK_CWD = '/home/ym/Documents/githubproject/learn-english/'
 WIN_WORKS_CWD = '/mywork/hubproject/learn-english/'
 
 # md 内容模板
+
+md_title = '# '
 md_content = '''
+
 ## 1.近义词
 
 ## 2.意异词
@@ -31,60 +34,55 @@ md_content = '''
 
 
 current_cwd = ''
-system_split_char = ''
-
-custome_cwd = ''
 
 
 def check_env():
     """判断当前运行环境，选择合适的路径"""
-    print('env: {}'.format(os.name))
-    print('path: {}'.format(os.getcwd()))
 
     # heck env
     global current_cwd
     if os.name == 'nt':
         current_cwd = WIN_WORKS_CWD[:]
-        system_split_char ='\\'
     else:
         current_cwd = LINUX_WORK_CWD[:]
-        system_split_char = '/'
 
     # check path
     if os.getcwd() != current_cwd:
         os.chdir(current_cwd)
+        print('env: {}'.format(os.name))
+        print('path: {}'.format(os.getcwd()))
 
 
 def create_file():
-    """生成指定语言类型文件
+    """生成 md 文件
     """
     print("--- creating problems files ---")
 
     local_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print('--- {} ---'.format(local_time))
-    file_menu = local_time[0:10].replace('-', '/')
-    print(file_menu)
+    print('--- now: {} ---'.format(local_time))
+    file_menu = local_time[0:10].replace('-', os.path.sep)
 
-    # if not os.path.exists(os.path.join(custome_cwd,file_year)):
+    try:
+        if not os.path.exists(file_menu[0:4]) and os.path.isdir(file_menu[0:4]):
+            os.mkdir(file_menu[0:4])
 
-    # 获取模板
-    # case_txt, test_txt = read_template()
-    # code_file = os.path.join(custome_cwd, 'case01.' +
-    #                          script_config.lang_config[sys.argv[2][1:]]['code'])
-    # test_file = os.path.join(custome_cwd, 'test.' +
-    #                          script_config.lang_config[sys.argv[2][1:]]['code'])
+        os.chdir(os.path.join(current_cwd, file_menu[0:4]))
+        if not os.path.exists(file_menu[5:7]) and os.path.isdir(file_menu[5:7]):
+            os.mkdir(file_menu[5:7])
 
-    # if not os.path.exists(code_file):
-    #     f_code = open(code_file, 'w')
-    #     f_code.write(case_txt)
-    #     f_code.close()
+        os.chdir(os.path.join(current_cwd, file_menu[0:7]))
+        if not os.path.exists(os.path.join(current_cwd, file_menu[8:10])):
+            with open(file_menu[8:10]+'.md', 'w', encoding='utf-8') as file:
+                file.write(
+                    md_title+str(file_menu[0:4])+"."+str(file_menu[5:7]))
+                file.write(md_content)
+                print("--- file: {} create success ---".format(file_menu))
 
-    # if not os.path.exists(test_file):
-    #     f_test = open(test_file, 'w')
-    #     f_test.write(test_txt)
-    #     f_test.close()
+    except Exception as e:
+        print("--- file: {} create fail ---".format(file_menu))
+        print(e)
 
-    # print("--- end ---")
+    print("--- end ---")
 
 
 if __name__ == '__main__':
