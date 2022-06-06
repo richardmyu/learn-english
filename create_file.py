62a8a0dcc9e8f873b7de80152d2f76ac9bbe14c4
yum <richardminyu@foxmail.com>
2022-06-06 15:08:41 +0800

    update script

--- create_file.py
+++ create_file.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """根据父级目录，生成 md 文件
 
 用法
@@ -6,18 +7,17 @@
 """
 
 import os
-import sys
-from threading import local
 import time
 
 # linux 和 windows 下不同的文件路径，若项目路径变动，此处也需修改
 LINUX_WORK_CWD = '/home/ym/Documents/githubproject/learn-english/'
 WIN_WORKS_CWD = '/mywork/hubproject/learn-english/'
 
-# md 内容模板
+CURRENT_CWD = ''
 
-md_title = '# '
-md_content = '''
+# md 内容模板
+MD_TITLE = '# '
+MD_CONTENT = '''
 
 ## 1.近义词
 
@@ -33,22 +33,19 @@ md_content = '''
 '''
 
 
-current_cwd = ''
-
-
 def check_env():
     """判断当前运行环境，选择合适的路径"""
 
     # heck env
-    global current_cwd
+    global CURRENT_CWD
     if os.name == 'nt':
-        current_cwd = WIN_WORKS_CWD[:]
+        CURRENT_CWD = WIN_WORKS_CWD[:]
     else:
-        current_cwd = LINUX_WORK_CWD[:]
+        CURRENT_CWD = LINUX_WORK_CWD[:]
 
     # check path
-    if os.getcwd() != current_cwd:
-        os.chdir(current_cwd)
+    if os.getcwd() != CURRENT_CWD:
+        os.chdir(CURRENT_CWD)
         print('env: {}'.format(os.name))
         print('path: {}'.format(os.getcwd()))
 
@@ -65,17 +62,17 @@ def create_file():
         # menu year
         if not os.path.exists(file_menu[0:4]) and os.path.isdir(file_menu[0:4]):
             os.mkdir(file_menu[0:4])
-        os.chdir(os.path.join(current_cwd, file_menu[0:4]))
+        os.chdir(os.path.join(CURRENT_CWD, file_menu[0:4]))
         # menu month
         if not os.path.exists(file_menu[5:7]) and os.path.isdir(file_menu[5:7]):
             os.mkdir(file_menu[5:7])
-        os.chdir(os.path.join(current_cwd, file_menu[0:7]))
+        os.chdir(os.path.join(CURRENT_CWD, file_menu[0:7]))
         # menu day
-        if not os.path.exists(os.path.join(current_cwd, file_menu[8:10])):
+        if not os.path.exists(os.path.join(CURRENT_CWD, file_menu[8:10])):
             with open(file_menu[8:10]+'.md', 'w', encoding='utf-8') as file:
                 file.write(
-                    md_title+str(file_menu[5:7])+"."+str(file_menu[8:10]))
-                file.write(md_content)
+                    MD_TITLE+str(file_menu[5:7])+"."+str(file_menu[8:10]))
+                file.write(MD_CONTENT)
                 print("--- file: {} create success ---".format(file_menu))
     except Exception as e:
         print("--- file: {} create fail ---".format(file_menu))
